f0c47b5f097170fc803450e0435fa4a6b8df3cc4
Stephen Li <me@ziyunli.dev>
2020-08-15 13:57:31 -0400

    hello world

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Hello {}
+
+impl Component for Hello {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! { <span>{"Hello world!"}</span> }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Hello>::new().mount_to_body();
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
